6d856f5525a9fd304368b6e3dbb8b098afb56812
Keylan King <kingkey0101@outlook.com>
2026-01-21 10:45:02 -0600

    Added/ getRecipeById.ts Lambda, rebuilt lambda_build folder for AWS

---
+++ lambda/getRecipeById.ts
@@ -0,0 +1,38 @@
+import { getRecipe } from "../handlers/recipes";
+
+export const handler = async (event: any) => {
+    try {
+
+
+        const id = event.pathParameters?.id;
+        if (!id) {
+            return {
+                statusCode: 400,
+                headers: { 'Content-Type': 'applications/json' },
+                body: JSON.stringify({ error: 'Recipe ID is required' })
+            };
+        }
+
+        const recipe = await getRecipe(id);
+        if (!recipe) {
+            return {
+                statusCode: 404,
+                headers: { 'Content-Type': 'applications/json' },
+                body: JSON.stringify({ error: 'Recipe not found' })
+            };
+        }
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'applications/json' },
+            body: JSON.stringify(recipe)
+        };
+    } catch (err) {
+        console.error('Error in getRecipe handler:', err);
+        return {
+            statusCode: 500,
+            headers: { 'Content-Type': 'applications/json' },
+            body: JSON.stringify({ error: 'Internal server error' })
+        }
+
+    }
+}
